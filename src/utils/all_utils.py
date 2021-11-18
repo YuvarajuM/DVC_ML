20364047ae11fc54ece0348ba5fafa8359185bd8
Yuvaraju M <vayu_yuva@yahoo.co.in>
2021-11-19 00:21:48 +0530

    stage split data file added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -9,3 +9,6 @@ def create_direcory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path,exist_ok=True)
         print(f"directory is created at {dir_path}")
+def save_local_df(data,data_path):
+    data.to_csv(data_path,index=False)
+    print(f"Data saved at {data_path}")
